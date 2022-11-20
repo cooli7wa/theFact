0303627a1fc54dec7e9e29821cb221a351e38968
cooli7wa <cooli7wa67@163.com>
2022-11-20 17:15:54 +0800

    script

--- _posts/_tools/site_deploy.sh
+++ _posts/_tools/site_deploy.sh
@@ -3,8 +3,8 @@
 sudo bundle update
 sudo jekyll build
 
-sudo rm -rf /var/www/cooli7wa.com
-sudo cp -R ./_site /var/www/cooli7wa.com
-sudo chmod -R a+r /var/www/cooli7wa.com
+sudo rm -rf /var/www/theFact.com
+sudo cp -R ./_site /var/www/theFact.com
+sudo chmod -R a+r /var/www/theFact.com
 
 sudo service nginx restart
